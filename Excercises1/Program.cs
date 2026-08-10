a5cabf1dd7826ad7bac9244fe343b1aa4e905c6c
Dime Azares <Dime Azares@DESKTOP-T2E58ET>
2026-08-10 18:11:15 +0800

    Add project files.

---
+++ Excercises1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Excercises1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
